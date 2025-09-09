fd584507f99317c442c9693c89c562d4fa84c860
obralinkca-hash <obralinkca@gmail.com>
2025-09-09 14:27:51 -0400

    Update LogoMarquee.tsx

--- components/LogoMarquee.tsx
+++ components/LogoMarquee.tsx
@@ -3,8 +3,10 @@ export default function LogoMarquee() {
   const logos = Array.from({ length: 6 }).map((_, i) => `/images/aliados/logo-${i+1}.svg`);
   return (
     <div className="overflow-hidden">
-      <div className="flex gap-12 animate-[marquee_30s_linear_infinite] opacity-70"
-           style={{maskImage: "linear-gradient(to right, transparent, black 10%, black 90%, transparent)"}}>
+      <div
+        className="flex gap-12 animate-[marquee_30s_linear_infinite] opacity-70"
+        style={{ maskImage: "linear-gradient(to right, transparent, black 10%, black 90%, transparent)" }}
+      >
         {[...logos, ...logos].map((src, i) => (
           // eslint-disable-next-line @next/next/no-img-element
           <img key={i} src={src} alt="Aliado de ObraLink" className="h-12 w-auto object-contain" />
